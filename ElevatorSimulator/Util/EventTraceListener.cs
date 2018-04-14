523caab593f53125438edd8e11a3157a1600ee7e
Josh <jdegraw42@gmail.com>
2018-04-13 21:45:13 -0600

    Removed TraceListener from Trace.Listeners
    That's where a lot of those crashes were coming from...

--- ElevatorSimulator/Util/EventTraceListener.cs
+++ ElevatorSimulator/Util/EventTraceListener.cs
@@ -4,13 +4,14 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ElevatorApp.Util
 {
     public class EventTraceListener : TraceListener
     {
-        private readonly object _locker = new object();
+        private readonly SemaphoreSlim mutex = new SemaphoreSlim(1);
 
         public string FileName { get; }
 
@@ -38,35 +39,41 @@ namespace ElevatorApp.Util
         {
             try
             {
-                lock (_locker)
+                mutex.Wait();
+
+                using (var writer = new StreamWriter(this.FileName, true, Encoding.ASCII))
                 {
-                    using (var writer = new StreamWriter(this.FileName, true, Encoding.ASCII))
-                    {
-                        writer.Write(message);
-                    }
+                    writer.Write(message);
                 }
+
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex);
+            }
+            finally
+            {
+                mutex.Release();
             }
         }
 
         public override void WriteLine(string message)
         {
+            mutex.Wait();
             try
             {
-                lock (_locker)
+                using (var writer = new StreamWriter(this.FileName, true, Encoding.ASCII))
                 {
-                    using (var writer = new StreamWriter(this.FileName, true, Encoding.ASCII))
-                    {
-                        writer.WriteLine(message);
-                    }
+                    writer.WriteLine(message);
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex);
+            }
+            finally
+            {
+                mutex.Release();
             }
         }
     }
